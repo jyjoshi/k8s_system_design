0c605f8a87a2ebfa0c740bfb6e61ba9693a838bb
Jay Joshi <jayjoshi112711@gmail.com>
2024-04-12 15:13:00 -0700

    Login route created

--- python/src/gateway/server.py
+++ python/src/gateway/server.py
@@ -15,3 +15,12 @@ fs = gridfs.GridFS(mongo.db)
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
 
+@server.route("/login", methods=["POST"])
+def login():
+    token, err = access.login(request)
+
+    if not err:
+        return token
+    else:
+        return err
+    
