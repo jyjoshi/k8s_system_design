d7d8fc5ff83c2c7a6d2585e7b9f024c773fa4c5d
Jay Joshi <jayjoshi112711@gmail.com>
2024-04-16 02:56:25 -0700

    Notification service created

---
+++ python/src/notification/consumer.py
@@ -0,0 +1,40 @@
+import pika, sys, os, time
+from send import email
+import logging
+
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+def main():
+    # rabbitmq connection
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='rabbitmq'),
+        )
+    channel = connection.channel()
+
+    def callback(ch, method, properties, body):
+        logging.warning(f"Received message: {body}")
+        err = email.notification(body)
+        if err:
+            logging.error(f"Error sending email: {err}")
+            ch.basic_nack(delivery_tag=method.delivery_tag)
+        else:
+            ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    channel.basic_consume(
+        queue=os.environ.get("MP3_QUEUE"),
+        on_message_callback=callback,
+    )
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+
+    channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
